4a2b5ebd952ac5002ff99676cf7fc300d29942e6
Matthias Bläsing <mblaesing@doppel-helix.eu>
2025-07-12 20:31:59 +0200

    Prepare CI/CD pipeline for github

--- src/test/docker/runtest.sh
+++ src/test/docker/runtest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Should be synced with <distro-root>/.github/workflows
+
 cd /clamd-client
 clamd --config-file src/test/docker/clamd.conf --log=/tmp/clamd.log
 mvn integration-test surefire-report:report-only surefire-report:failsafe-report-only verify
