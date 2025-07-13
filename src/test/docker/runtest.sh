17d9ed1ad7eba8cfe29df0d0b583cc7fcb9d36ad
Matthias Bläsing <mblaesing@doppel-helix.eu>
2025-07-13 13:43:27 +0200

    Prepare release to maven central

--- src/test/docker/runtest.sh
+++ src/test/docker/runtest.sh
@@ -5,4 +5,4 @@
 cd /clamd-client
 clamd --config-file src/test/docker/clamd.conf --log=/tmp/clamd.log
 mvn --no-transfer-progress -DskipTests install
-mvn --no-transfer-progress integration-test surefire-report:report-only surefire-report:failsafe-report-only verify
+mvn --no-transfer-progress -Prun-it integration-test surefire-report:report-only surefire-report:failsafe-report-only verify
